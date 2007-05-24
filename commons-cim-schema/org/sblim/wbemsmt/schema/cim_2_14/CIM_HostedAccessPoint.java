4ac6e1d4c882d1eb504c03d78f82737592f85e6a
mbauschert <mbauschert>
2007-05-24 08:23:31 +0000

    Fix [ 1724668 ] wbemsmt-commons: add network-classes to cimschema 2.14

---
+++ commons-cim-schema/org/sblim/wbemsmt/schema/cim_2_14/CIM_HostedAccessPoint.java
@@ -0,0 +1,431 @@
+/** 
+ * CIM_HostedAccessPoint.java
+ *
+ * © Copyright IBM Corp. 2005
+ *
+ * THIS FILE IS PROVIDED UNDER THE TERMS OF THE COMMON PUBLIC LICENSE
+ * ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE
+ * CONSTITUTES RECIPIENTS ACCEPTANCE OF THE AGREEMENT.
+ *
+ * You can obtain a current copy of the Common Public License from
+ * http://www.opensource.org/licenses/cpl1.0.php
+ *
+ * @author:	ECCG 0.9.7 generated 
+ * 			(author should be changed, e.g. First and Last Name <xxx@cc.ibm.com>)
+ *
+ * Contributors:
+ *
+ *
+ * Description:  CIM_HostedAccessPoint is an association between a Service AccessPoint and the
+ * System on which it is provided. The cardinality of this association is
+ * one-to-many and is weak with respect to the System. Each System can host many
+ * ServiceAccessPoints. Heuristic: If the implementation of the
+ * ServiceAccessPoint is modeled, it must be implemented by a Device or
+ * SoftwareFeature that is part of the System that is hosting the
+ * ServiceAccessPoint.
+ * 
+ */
+
+package org.sblim.wbemsmt.schema.cim_2_14;
+
+import java.security.InvalidParameterException;
+import java.util.Vector;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import org.sblim.wbem.cim.*;
+
+
+/**
+ *  CIM_HostedAccessPoint is an association between a Service AccessPoint and the
+ * System on which it is provided. The cardinality of this association is
+ * one-to-many and is weak with respect to the System. Each System can host many
+ * ServiceAccessPoints. Heuristic: If the implementation of the
+ * ServiceAccessPoint is modeled, it must be implemented by a Device or
+ * SoftwareFeature that is part of the System that is hosting the
+ * ServiceAccessPoint.
+ */
+public class CIM_HostedAccessPoint extends CIM_HostedDependency  {
+	
+	public final static String CIM_CLASS_NAME = "CIM_HostedAccessPoint"; //$NON-NLS-1$
+	public final static String CIM_CLASS_DISPLAYNAME = CIM_CLASS_NAME;
+
+	private boolean validCimInstance = false;
+	
+	public final static String CIM_CLASS_VERSION = "2.10.0";
+	public final static String CIM_PROPERTY_CIM_SYSTEM = "Antecedent"; //$NON-NLS-1$
+	public final static String CIM_PROPERTY_CIM_SERVICEACCESSPOINT = "Dependent"; //$NON-NLS-1$
+	
+	
+	
+	
+	
+
+	public static Vector CIM_PropertyNameList	= new Vector();
+	public static Vector CIM_PropertyList 		= new Vector();
+	private static Set Java_Package_List 		= new HashSet();
+	
+	static {
+		CIM_PropertyNameList.add(CIM_PROPERTY_CIM_SYSTEM);
+		CIM_PropertyNameList.add(CIM_PROPERTY_CIM_SERVICEACCESSPOINT);
+				
+		for (int i = 0; i < CIM_HostedDependency.CIM_PropertyNameList.size(); i++) {
+			
+			CIM_HostedAccessPoint.CIM_PropertyNameList.add(CIM_HostedDependency.CIM_PropertyNameList.elementAt(i));
+		}
+		
+		
+		CIM_PropertyList.add(new CIMProperty(CIM_PROPERTY_CIM_SYSTEM, new CIMValue(null, new CIMDataType(CIM_System.CIM_CLASS_NAME))));
+		CIM_PropertyList.add(new CIMProperty(CIM_PROPERTY_CIM_SERVICEACCESSPOINT, new CIMValue(null, new CIMDataType(CIM_ServiceAccessPoint.CIM_CLASS_NAME))));
+				
+		for (int i = 0; i < CIM_HostedDependency.CIM_PropertyList.size(); i++) {
+			
+			CIM_HostedAccessPoint.CIM_PropertyList.add(CIM_HostedDependency.CIM_PropertyList.elementAt(i));
+		}
+		
+		addPackage("org.sblim.wbemsmt.schema.cim_2_14");
+				
+		String[] parentClassPackageList = CIM_HostedDependency.getPackages();
+		
+		for (int i = 0; i < parentClassPackageList.length; i++) {
+			Java_Package_List.add(parentClassPackageList[i]);
+		}
+	};
+			
+	
+	
+	
+	
+	//**********************************************************************
+	// Constructors 	
+	//**********************************************************************
+
+	/**
+	*	Class constructor
+	*/	
+	public CIM_HostedAccessPoint() {
+
+		this.cimInstance	= new CIMInstance();
+		
+		for (int i = 0; i < CIM_PropertyList.size(); i++) {
+			this.cimInstance.addProperty((CIMProperty)((CIMProperty)CIM_PropertyList.get(i)).clone());
+		}
+		
+		this.cimObjectPath 	= new CIMObjectPath(CIM_CLASS_NAME);
+		this.cimInstance.setObjectPath(this.cimObjectPath);
+
+		this.original_cimInstance	= (CIMInstance)this.cimInstance.clone();
+
+		setValidCimInstance(false);
+	}
+
+
+	/**
+	*	Class constructor
+	*/	
+	public CIM_HostedAccessPoint(Vector keyProperties){ 
+		this();
+		
+		if (keyProperties == null) {
+			throw new InvalidParameterException("The keyProperties parameter does not contain a valid reference.");
+		
+		}
+		
+		Iterator iter = keyProperties.iterator();
+		while (iter.hasNext()) {
+			Object property = iter.next();
+			
+			if (property instanceof CIMProperty) {
+				CIMProperty keyProperty = (CIMProperty)property;
+				this.cimObjectPath.addKey(keyProperty);
+				
+				if (this.cimInstance.getProperty(keyProperty.getName()) != null) {
+					this.cimInstance.removeProperty(keyProperty.getName());
+				}
+				this.cimInstance.addProperty(keyProperty);
+				
+			} else {
+				throw new InvalidParameterException("The keyProperties parameter should only contain objects of class CIMProperty.");
+				
+			}
+		}
+		
+		setValidCimInstance(false);
+	}
+
+	
+	/**
+	*	Class constructor
+	*/	
+	public CIM_HostedAccessPoint(CIMObjectPath cimObjectPath, CIMInstance cimInstance){ 
+		
+		if (cimInstance == null) {
+			throw new InvalidParameterException("The cimInstance parameter does not contain a valid reference.");
+		
+		} else if (cimObjectPath == null){
+			throw new InvalidParameterException("The cimObjectPath parameter does not contain a valid reference.");		
+		
+		} else if (!cimObjectPath.getObjectName().equals(cimInstance.getClassName())) {
+			throw new InvalidParameterException("The class name of the instance and the ObjectPath are not the same.");
+		}
+		
+		setCimInstance(cimInstance);
+		this.original_cimInstance = (CIMInstance)cimInstance.clone();
+		this.cimObjectPath        = cimObjectPath;
+		setValidCimInstance(true);
+	}
+
+	
+	/**
+	*	The method returns the display name of the class
+	*/	
+	public String getClassDisplayName(){
+		return CIM_CLASS_DISPLAYNAME;
+	}
+	
+	public static void addPackage(String packagename) {
+        if (packagename != null) {
+            if (!packagename.endsWith(".")) {
+                packagename = packagename + ".";
+            }
+            CIM_HostedAccessPoint.Java_Package_List.add(packagename);
+            
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    public static String[] getPackages() {
+        return (String[]) CIM_HostedAccessPoint.Java_Package_List.toArray(new String[CIM_HostedAccessPoint.Java_Package_List.size()]);
+    }
+	
+	//**********************************************************************
+	// Instance methods
+	//**********************************************************************
+
+	/**
+	*	no description
+	*/	
+	public boolean isDataValid(Vector invalidProperties) {
+		boolean result = true;
+		
+		if (invalidProperties == null) {
+			invalidProperties = new Vector();
+		} else {
+			invalidProperties.removeAllElements();
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * @return Returns the validCimInstance.
+	 */
+	public boolean isValidCimInstance() {
+		return this.validCimInstance;
+	}
+	
+	/**
+	 * @param validCimInstance The validCimInstance to set.
+	 */
+	private void setValidCimInstance(boolean isValidCimInstance) {
+
+		this.validCimInstance = isValidCimInstance;
+	}
+	
+	
+	/**
+	*	The method returns this CIM instance
+	*/
+	public CIMInstance getCimInstance() {
+		
+		return this.cimInstance;
+	}
+	
+	
+	/**
+	*	The method sets this CIM instance
+	*/
+	public void setCimInstance(CIMInstance cimInstance) {
+		
+		this.cimInstance = cimInstance;
+	}
+	
+		
+	/**
+	*	The method returns this CIM object path
+	*/
+	public CIMObjectPath getCimObjectPath() {
+		return this.cimObjectPath;
+	}
+	
+	
+	/**
+	*	The method checks if the cimInstance has been modified
+	*/
+	public boolean isModified() {
+	    
+	    if (!this.original_cimInstance.equals(this.cimInstance)) 
+	        return true;
+	    
+	    return false;
+	}
+	
+	
+	/**
+	*	The method resets the values of the cimInstance
+	*/	
+	public void resetValues() {
+	    this.cimInstance = (CIMInstance)this.original_cimInstance.clone();
+	}
+	
+	
+	/**
+	*	The method checks if the cimInstance equals an other cimInstance
+	*/	
+	public boolean equals(Object object) {
+	    
+	    if (!(object instanceof CIM_HostedAccessPoint)) {
+	        return false;
+	    }
+	    
+	    if (this.cimInstance == null && ((CIM_HostedAccessPoint)object).cimInstance != null) {
+	    	return false;
+	    	
+	    } else if (this.cimInstance != null && ((CIM_HostedAccessPoint)object).cimInstance == null) {
+	    	return false;
+	    	
+	    } else if (this.cimInstance != null && !this.cimInstance.equals(((CIM_HostedAccessPoint)object).cimInstance)) {
+	    	return false;
+	    	
+	    } else if (this.original_cimInstance == null && ((CIM_HostedAccessPoint)object).original_cimInstance != null) {
+	    	return false;
+	    	
+	    } else if (this.original_cimInstance != null && ((CIM_HostedAccessPoint)object).original_cimInstance == null) {
+	    	return false;
+	    	
+	    } else if (this.original_cimInstance != null && !this.original_cimInstance.equals(((CIM_HostedAccessPoint)object).original_cimInstance)) {
+	        return false;
+	        
+	    } else if (this.cimObjectPath == null && ((CIM_HostedAccessPoint)object).cimObjectPath != null) {
+	    	return false;
+	    	
+	    } else if (this.cimObjectPath != null && ((CIM_HostedAccessPoint)object).cimObjectPath == null) {
+	    	return false;
+		    	
+	    } else if (this.cimObjectPath != null && !this.cimObjectPath.equals(((CIM_HostedAccessPoint)object).cimObjectPath)) {
+	        return false;
+	        
+	    } 
+	    
+	    return true;
+	}
+	
+	/**
+	*	The method return this method as a string
+	*/	
+	public String toString() {
+		return this.cimInstance.toString();
+	}
+
+	//*****************************************************
+	// Associators methods
+	//*****************************************************
+	
+	
+	
+	//*****************************************************
+	// Attribute methods
+	//*****************************************************
+	
+	// Attribute CIM_System
+	
+	public CIMObjectPath get_CIM_System() {
+		
+		CIMProperty currentProperty = this.cimInstance.getProperty(CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SYSTEM);
+        
+		if (currentProperty == null) {
+			throw new CIMException(CIMException.CIM_ERR_NO_SUCH_PROPERTY, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SYSTEM + " could not be found");
+    		
+		} else if (currentProperty.getType() == null ) {
+			throw new CIMException(CIMException.CIM_ERR_TYPE_MISMATCH, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SYSTEM + " is not of expected type CIM_System.");
+		}
+        
+		if (currentProperty.getValue() == null) {
+			return null;
+		}
+        
+		return (CIMObjectPath)currentProperty.getValue().getValue();
+	}
+	    
+			
+	public void set_CIM_System(CIM_System newValue) {
+		
+		CIMProperty currentProperty = this.cimInstance.getProperty(CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SYSTEM);
+    	
+		if (currentProperty == null) {
+			throw new CIMException(CIMException.CIM_ERR_NO_SUCH_PROPERTY, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SYSTEM + " could not be found");
+    		
+		} else if (!CIM_HostedAccessPointHelper.isValid_CIM_System(newValue)) {
+			throw new InvalidParameterException("The value " + newValue + " is not valid for property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SYSTEM);
+    		
+		} else if (currentProperty.getType() == null ) {
+			throw new CIMException(CIMException.CIM_ERR_TYPE_MISMATCH, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SYSTEM + " is not of expected type CIM_System.");
+		}
+    	
+		CIMValue updatedValue = new CIMValue(newValue.getCimObjectPath(), new CIMDataType(CIM_System.CIM_CLASS_NAME));
+		currentProperty.setValue(updatedValue);
+	}	
+	    
+	
+
+	// Attribute CIM_ServiceAccessPoint
+	
+	public CIMObjectPath get_CIM_ServiceAccessPoint() {
+		
+		CIMProperty currentProperty = this.cimInstance.getProperty(CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SERVICEACCESSPOINT);
+        
+		if (currentProperty == null) {
+			throw new CIMException(CIMException.CIM_ERR_NO_SUCH_PROPERTY, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SERVICEACCESSPOINT + " could not be found");
+    		
+		} else if (currentProperty.getType() == null ) {
+			throw new CIMException(CIMException.CIM_ERR_TYPE_MISMATCH, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SERVICEACCESSPOINT + " is not of expected type CIM_ServiceAccessPoint.");
+		}
+        
+		if (currentProperty.getValue() == null) {
+			return null;
+		}
+        
+		return (CIMObjectPath)currentProperty.getValue().getValue();
+	}
+	    
+			
+	public void set_CIM_ServiceAccessPoint(CIM_ServiceAccessPoint newValue) {
+		
+		CIMProperty currentProperty = this.cimInstance.getProperty(CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SERVICEACCESSPOINT);
+    	
+		if (currentProperty == null) {
+			throw new CIMException(CIMException.CIM_ERR_NO_SUCH_PROPERTY, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SERVICEACCESSPOINT + " could not be found");
+    		
+		} else if (!CIM_HostedAccessPointHelper.isValid_CIM_ServiceAccessPoint(newValue)) {
+			throw new InvalidParameterException("The value " + newValue + " is not valid for property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SERVICEACCESSPOINT);
+    		
+		} else if (currentProperty.getType() == null ) {
+			throw new CIMException(CIMException.CIM_ERR_TYPE_MISMATCH, "The property " + CIM_HostedAccessPoint.CIM_PROPERTY_CIM_SERVICEACCESSPOINT + " is not of expected type CIM_ServiceAccessPoint.");
+		}
+    	
+		CIMValue updatedValue = new CIMValue(newValue.getCimObjectPath(), new CIMDataType(CIM_ServiceAccessPoint.CIM_CLASS_NAME));
+		currentProperty.setValue(updatedValue);
+	}	
+	    
+	
+
+	
+	
+	//*****************************************************
+	// Invoke methods
+	//*****************************************************
+	
+	
+
+}
