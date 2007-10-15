c383c2c8d09b9abaac15fecf7705b2b38b950c12
mbauschert <mbauschert>
2007-10-15 15:47:56 +0000

    fix 1813917  wbemsmt-commons: util methods for embedded instances

--- commons-bl/org/sblim/wbemsmt/bl/FcoUtil.java
+++ commons-bl/org/sblim/wbemsmt/bl/FcoUtil.java
@@ -20,11 +20,20 @@
 package org.sblim.wbemsmt.bl;
 
 import java.lang.reflect.Method;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.lang.StringUtils;
+import org.sblim.wbem.cim.CIMInstance;
+import org.sblim.wbem.cim.CIMObjectPath;
+import org.sblim.wbem.cim.CIMProperty;
+import org.sblim.wbem.cimxml.CIMXmlUtil;
+import org.sblim.wbem.cimxml.CIMXmlUtilFactory;
+import org.sblim.wbem.client.CIMClient;
 import org.sblim.wbemsmt.bl.adapter.DataContainer;
+import org.sblim.wbemsmt.bl.fco.CIM_ObjectIf;
+import org.sblim.wbemsmt.exception.ModelLoadException;
 import org.sblim.wbemsmt.tools.input.LabeledBaseInputComponentIf;
 
 public class FcoUtil {
@@ -81,6 +90,117 @@ public class FcoUtil {
 			throw new IllegalArgumentException("Cannot copy values to datacontainer " + e.getMessage());
 		} 
 		
+	}
+	
+	public static Object getFirstChild(Class mustReturnThis, List list, boolean silent, boolean createIfNotExists, CIMClient client) throws ModelLoadException {
+		if (!silent && list.size() != 1)
+		{
+			logger.severe("Cannot get Element of Type " + mustReturnThis.getName() + " beause not exact one element was found in List. Found: " + list.size());
+			throw new ModelLoadException("Cannot load data model");
+		}
+		
+		if (!silent && list.get(0) == null)
+		{
+			logger.severe("Cannot get Element of Type " + mustReturnThis.getName() + " beause first element was null");
+			throw new ModelLoadException("Cannot load data model");
+		}
+	
+		if (!silent && !mustReturnThis.isAssignableFrom(list.get(0).getClass()) && !(list.get(0) instanceof CIMObjectPath) )
+		{
+			logger.severe("Cannot get Element of Type " + mustReturnThis.getName() + " beause first element is not from type " + mustReturnThis.getName() + "  but is of type " + list.get(0).getClass());
+			throw new ModelLoadException("Cannot load data model");
+		}
+		
+		if (list.size() == 0 && createIfNotExists)
+		{
+			try {
+				return mustReturnThis.newInstance();
+			} catch (Exception e) {
+				throw new ModelLoadException(e);
+			}
+		}
+		
+		if (silent && list.size() == 0)
+		{
+			return null;
+		}
+		else
+		{
+			Object o = list.get(0);
+			if (o instanceof CIMObjectPath)
+			{
+				CIMObjectPath path = (CIMObjectPath) o;
+				String helper = mustReturnThis.getName() + "Helper";
+				Object fco = null;
+				try {
+					Class clsHelper = Class.forName(helper);
+					Method method = clsHelper.getMethod("getInstance",new Class[]{CIMClient.class,CIMObjectPath.class});
+					fco = method.invoke(null,new Object[]{client,path});
+					return fco;
+				} catch (Exception e) {
+					throw new ModelLoadException("Cannot load element with path " + path, e);
+				}
+			}
+			else
+			{
+				return o;
+			}
+		}
+	}
+
+
+	
+	/**
+	 * Create a embedded instance (a cim object as xml representation) with all attributes
+	 * @param cimObject
+	 * @return
+	 */
+	public static String getEmbeddedInstance(CIM_ObjectIf cimObject) {
+		return getEmbeddedInstance(cimObject, false);
+	}
+	
+	/**
+	 * Create a embedded instance (a cim object as xml representation)
+	 * @param cimObject
+	 * @param onlyKeys set to true if the instance should contain only key properties
+	 * @return
+	 */
+	public static String getEmbeddedInstance(CIM_ObjectIf cimObject, boolean onlyKeys) {
+		CIMInstance cimInstance = cimObject.getCimInstance();
+		if (onlyKeys)
+		{
+			//we need to clone the object because we are removing the non-key-properties
+			cimInstance = (CIMInstance) cimInstance.clone();
+
+			//collect the key properties
+			Vector keys = cimObject.getCimObjectPath().getKeys();
+			Set keynames = new HashSet();
+			for (Iterator iterator = keys.iterator(); iterator.hasNext();) {
+				CIMProperty property = (CIMProperty) iterator.next();
+				keynames.add(property.getName());
+				
+			}
+
+			//remove the non-key properties
+			Vector properties = cimInstance.getAllProperties();
+			for (int i = properties.size() - 1; i >= 0; i--) {
+				CIMProperty property = (CIMProperty) properties.get(i);
+				String propertyName = property.getName();
+				if (!keynames.contains(propertyName))
+				{
+					cimInstance.removeProperty(propertyName);
+				}
+			}
+			
+		}
+		
+		//create the XML
+		CIMXmlUtil util = CIMXmlUtilFactory.getCIMXmlUtil();
+		String xml = util.CIMElementToXml(cimInstance);
+		
+		//cut of the first two lines - <xml...> and a empty line
+		xml = xml.substring(xml.indexOf("<INSTA"));
+		return xml;
 	}
 
 }
