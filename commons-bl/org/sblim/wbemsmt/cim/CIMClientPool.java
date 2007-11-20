096b9c817fb5200ce7292d504ee141305ac941a1
mbauschert <mbauschert>
2007-11-20 07:21:14 +0000

    Fix 1834954  wbemsmt-commons: cimclientpool.getJsr48Client

--- commons-bl/org/sblim/wbemsmt/cim/CIMClientPool.java
+++ commons-bl/org/sblim/wbemsmt/cim/CIMClientPool.java
@@ -20,8 +20,14 @@
   */
 package org.sblim.wbemsmt.cim;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+
+import javax.security.auth.Subject;
 
 import org.sblim.wbem.cim.CIMException;
 import org.sblim.wbem.cim.CIMNameSpace;
@@ -38,6 +44,7 @@ public class CIMClientPool {
 	String port;
 
 	Map clientsByNamespace = new HashMap(); 
+	Map jsr48ClientsByNamespace = new HashMap(); 
 	
 	public CIMClientPool(String hostname, String port, String username, char[] password) {
 		super();
@@ -61,20 +68,102 @@ public class CIMClientPool {
 		CIMClient client = (CIMClient) clientsByNamespace.get(namespace);
 		if (client == null)
 		{
-			String url = "HTTP://" + hostname + ":" + port.trim();
-			client = new CIMClient(new CIMNameSpace(url,namespace), new UserPrincipal(username.trim()), new PasswordCredential(password));
-			//check if the client can access the server
-			try {
-				client.enumerateClasses();
-			} catch (CIMException e) {
-				throw new LoginException(e,client);
-			}
-			clientsByNamespace.put(namespace, client);
+			client = getNewCIMClient(namespace);
 		}
 		
 		return client;
 		
 	}
+
+	/**
+	 * Creates a new CIMClient. Not uses the cached version
+	 * @param namespace
+	 * @return
+	 * @throws LoginException
+	 */
+
+	public CIMClient getNewCIMClient(String namespace) throws LoginException {
+		CIMClient client;
+		String url = "HTTP://" + hostname + ":" + port.trim();
+		client = new CIMClient(new CIMNameSpace(url,namespace), new UserPrincipal(username.trim()), new PasswordCredential(password));
+		//check if the client can access the server
+		try {
+			client.enumerateClasses();
+		} catch (CIMException e) {
+			throw new LoginException(e,client);
+		}
+		clientsByNamespace.put(namespace, client);
+		return client;
+	}
+	
+	/**
+	 * get the jsr48 cimclient for the given namespace
+	 * 
+	 * if for the namespace no cimClient exists a new one is created
+	 * 
+	 * @param namespace
+	 * @return the javax.wbem.client.WBEMClient (as object because we wanted no dependency to the jsr48 interfaces)   
+	 */
+	public Object getJsr48CIMClient(String namespace)  throws LoginException {
+	
+		
+		Object result = jsr48ClientsByNamespace.get(namespace);
+		if (result == null)
+		{
+			try {
+				result = getNewJsr48CIMClient(namespace);
+				jsr48ClientsByNamespace.put(namespace, result);
+			} catch (Exception e) {
+				throw new LoginException("Cannot create JSR48 client",e,null);
+			}
+		}
+		
+		return result;
+	}
+
+
+	/**
+	 * Creates a new JSR48CIMClient. Not uses the cached version 
+	 * @param namespace
+	 * @return
+	 * @throws Exception
+	 */
+	public Object getNewJsr48CIMClient(String namespace) throws Exception {
+		Object result;
+		URL pWbemUrl = new URL("HTTP://" + hostname + ":" + port.trim());
+		//result = new CIMClient(new CIMNameSpace(url,namespace), new UserPrincipal(username.trim()), new PasswordCredential(password));
+
+		Class factory = Class.forName("javax.wbem.client.WBEMClientFactory");
+		Class path = Class.forName("javax.cim.CIMObjectPath");
+		
+		
+		Class property = Class.forName("javax.cim.CIMProperty");
+		property = Array.newInstance(property, 0).getClass();
+		Class userprincipal = Class.forName("javax.wbem.client.UserPrincipal");
+		Class passwordCredential = Class.forName("javax.wbem.client.PasswordCredential");
+		Class clsClient = Class.forName("javax.wbem.client.WBEMClient");
+		
+		Object userPrincipalObject = userprincipal.getConstructor(new Class[]{String.class}).newInstance(new Object[]{username});
+		Object passwordCredentialObject = passwordCredential.getConstructor(new Class[]{String.class}).newInstance(new Object[]{String.valueOf(password)});
+		
+		Method m = factory.getMethod("getClient", new Class[]{String.class});
+		
+		result = m.invoke(factory, new Object[]{"CIM-XML"});
+		
+		Object pathObject = path.getConstructor(new Class[]{String.class, String.class, String.class, String.class,String.class, property})
+			.newInstance(new Object[]{pWbemUrl.getProtocol(),pWbemUrl.getHost(), String.valueOf(pWbemUrl.getPort()), null, null, null});
+		
+		final Subject subject = new Subject();
+		subject.getPrincipals().add(userPrincipalObject);
+		subject.getPrivateCredentials().add(passwordCredentialObject);
+		
+		clsClient.getMethod("initialize", new Class[]{path,subject.getClass(),Locale[].class}).invoke(result, new Object[]{pathObject, subject, new Locale[] { Locale.US }});
+
+		pathObject = path.getConstructor(new Class[]{String.class, String.class}).newInstance(new Object[]{"CIM_ManagedElement",namespace});
+		
+		result.getClass().getMethod("enumerateClassNames", new Class[]{path,boolean.class}).invoke(result, new Object[]{pathObject,Boolean.TRUE});
+		return result;
+	}	
 
 	/**
 	 * return a new cimClient if the namespace of the cimclient and the namespace if the objectPath are not the same
@@ -97,4 +186,15 @@ public class CIMClientPool {
 			return getCIMClient(ns2);
 		}
 	}
+	
+	/**
+	 * return a jsr48 cimClient for the namespace within the objectPath
+	 * @param cimObjectPath carrying the namespace
+	 * @return the javax.wbem.client.WBEMClient (as object because we wanted no dependency to the jsr48 interfaces)
+	 */
+
+	public Object getJsr48CIMClient(CIMObjectPath cimObjectPath) throws LoginException {
+		String ns = cimObjectPath.getNameSpace();
+		return getJsr48CIMClient(ns);
+	}	
 }
--- sblim-wbemsmt-commons.spec
+++ sblim-wbemsmt-commons.spec
@@ -126,8 +126,9 @@ mv target/package/help/* $RPM_BUILD_ROOT%{_localstatedir}/lib/sblim-wbemsmt/help
 
 ###############################################################################
 %changelog
-* Tue Nov 13 2007 Michael Bauschert <michael.bauschert@de.ibm.com> 0.5.2-CVS
+* Tue Nov 20 2007 Michael Bauschert <michael.bauschert@de.ibm.com> 0.5.2-CVS
   - Inclusion of fixes for the following issues:
+    o 1834954  wbemsmt-commons: cimclientpool.getJsr48Client
     o 1830999  wbemsmt-commons: return DestinationManager for CLI
     o 1826622  wbemsmt-commons-bl: getEmbeddedInstance from CIMInstance
     o 1821943  wbemsmt-commons-test: reduce tracing while client creation
